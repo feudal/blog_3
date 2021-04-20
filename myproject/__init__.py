431839bedd6e1564ba0e4691981af2f8c090c3e2
calmis <calmis87@gmail.com>
2021-04-20 13:16:11 +0300

    models setup

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,7 +1,27 @@
+import os
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
+
+###################################################################################
+# DATABASE SETUP
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+Migrate(app.db)
+###################################################################################
+#  LOGIN CONFIGS
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+###################################################################################
 
 from myproject.core.views import core
 from myproject.error_pages.handlers import error_pages
