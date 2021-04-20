6c787a06ee091447890c9b72ee52889c06783af2
calmis <calmis87@gmail.com>
2021-04-20 12:42:47 +0300

    error pages

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -4,4 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from myproject.core.views import core
+from myproject.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
